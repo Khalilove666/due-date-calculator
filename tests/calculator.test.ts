716b5e847ee16bd3120499e889e5f251bb25eabd
Elgun Khalilov <elgun@Elguns-MacBook-Air.local>
2025-05-22 02:20:15 +0200

    fix: test cases naming

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -6,31 +6,31 @@ import {
 } from "../src/calculator";
 
 describe("calculateDueDate", () => {
-  it("should return a valid due date", () => {
+  it("should add 1 day", () => {
     expect(calculateDueDate("2025-05-20T10:00:00Z", 8)).toEqual(
       "2025-05-21T10:00:00.000Z"
     );
   });
 
-  it("should return a valid due date", () => {
+  it("should handle adding a weekend and extra hours", () => {
     expect(calculateDueDate("2025-05-20T16:45:00Z", 45)).toEqual(
       "2025-05-28T13:45:00.000Z"
     );
   });
 
-  it("should return a valid due date", () => {
+  it("should handle adding several weeks: 102 hours difference", () => {
     expect(calculateDueDate("2025-05-21T16:00:00Z", 102)).toEqual(
       "2025-06-09T14:00:00.000Z"
     );
   });
 
-  it("should return a valid due date", () => {
+  it("should return 9:00 as due not 17:00", () => {
     expect(calculateDueDate("2025-05-21T16:00:00Z", 97)).toEqual(
       "2025-06-09T09:00:00.000Z"
     );
   });
 
-  it("should return a valid due date", () => {
+  it("should handle partial hours: 0.5 hour", () => {
     expect(calculateDueDate("2025-05-21T16:00:00Z", 8.5)).toEqual(
       "2025-05-22T16:30:00.000Z"
     );
